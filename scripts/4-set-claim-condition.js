136254b6c39287208c0e209a17ebcfb665b530a2
Matt Lilley <m.k.lilley@gmail.com>
2022-01-27 09:27:39 +0000

    Set conditions for users to claim the membership NFT

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,25 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x2A4B55fde7e2a093ebc1dc0A72cE33fbdD164d5A"
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    // Specify conditions.
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 8_000_000_000,
+      maxQuantityPerTransaction: 1,
+    });
+
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log(
+      "✅ Successfully set claim condition on bundle drop:",
+      bundleDrop.address
+    );
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})();
